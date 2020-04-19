45e7e8f7ee8b2391b2a41052024eaa9ae12bd04f
Osama Aldemeery <aldemeery@gmail.com>
2020-04-19 17:49:54 +0200

    Disable composer process timeout.

--- index.php
+++ index.php
@@ -6,11 +6,14 @@ use GuzzleHttp\Cookie\CookieJar;
 
 require_once(__DIR__ . "/vendor/autoload.php");
 
+set_time_limit(0);
 date_default_timezone_set(config('timezone'));
-$checker = new Checker(new Client(config('client')), new CookieJar());
 
 try {
-    $checker->run();
+    (new Checker(
+        new Client(config('client')),
+        new CookieJar())
+    )->run();
 } catch (Exception $e) {
     file_put_contents(__DIR__ . "/logs/" . date("Y-m-d_h-i-s") . ".log", $e);
 }
